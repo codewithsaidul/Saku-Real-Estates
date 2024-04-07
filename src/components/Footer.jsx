ce7976117055791e7b052816dffc121df65c026e
codewithsaidul <codewithsaidul@gmail.com>
2024-04-08 05:05:13 +0600

    Footer Component Updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,29 +5,47 @@ import { FaFacebook, FaGithub, FaInstagram, FaTwitter } from "react-icons/fa";
 const Footer = () => {
   return (
     <div className="mt-20 bg-black text-white">
-      <footer className="max-w-[1170px] mx-auto py-20">
+      <footer className="max-w-[1170px] mx-auto py-20 px-4 text-center lg:text-left">
         <div className="flex flex-col lg:flex-row justify-between items-center gap-16">
-          <div>
-            <h1 className="text-5xl font-bold mb-7">SakuBliss Estates</h1>
-            <p className="text-lg font-medium text-gray-500 w-[500px]">
+          <div className="mt-5">
+            <h1 className="text-3xl md:text-5xl font-bold mb-7">
+              SakuBliss Estates
+            </h1>
+            <p className="text-lg font-medium text-gray-500 w-full md:w-[500px]">
               SakuBliss Estates is a wonderful choice! It&apos;s memorable,
               elegant, and evokes a sense of comfort and contentment. With this
               name, potential visitors will likely anticipate finding luxurious
               and peaceful living spaces on your website.
             </p>
 
-            <div className="mt-7 flex gap-x-5">
-              <a href="https://facebook.com/codewithsaidulwr" target="_blank" className="text-[#1D5D9B] font-bold border border-[#1D5D9B] p-5 rounded-full hover:text-white hover:bg-[#1D5D9B] duration-1000">
-                <FaFacebook size={24}/>
+            <div className="mt-3 flex gap-x-5 justify-center md:justify-start">
+              <a
+                href="https://facebook.com/codewithsaidulwr"
+                target="_blank"
+                className="text-[#1D5D9B] font-bold border border-[#1D5D9B] p-5 rounded-full hover:text-white hover:bg-[#1D5D9B] duration-1000"
+              >
+                <FaFacebook size={24} />
               </a>
-              <a href="https://twitter.com/codewithsaidul" target="_blank" className="text-[#1D5D9B] font-bold border border-[#1D5D9B] p-5 rounded-full hover:text-white hover:bg-[#1D5D9B] duration-1000">
-                <FaTwitter size={24}/>
+              <a
+                href="https://twitter.com/codewithsaidul"
+                target="_blank"
+                className="text-[#1D5D9B] font-bold border border-[#1D5D9B] p-5 rounded-full hover:text-white hover:bg-[#1D5D9B] duration-1000"
+              >
+                <FaTwitter size={24} />
               </a>
-              <a href="https://instagram.com/codewithsaidul" target="_blank" className="text-[#1D5D9B] font-bold border border-[#1D5D9B] p-5 rounded-full hover:text-white hover:bg-[#1D5D9B] duration-1000">
-                <FaInstagram size={24}/>
+              <a
+                href="https://instagram.com/codewithsaidul"
+                target="_blank"
+                className="text-[#1D5D9B] font-bold border border-[#1D5D9B] p-5 rounded-full hover:text-white hover:bg-[#1D5D9B] duration-1000"
+              >
+                <FaInstagram size={24} />
               </a>
-              <a href="https://github.com/codewithsaidul" target="_blank" className="text-[#1D5D9B] font-bold border border-[#1D5D9B] p-5 rounded-full hover:text-white hover:bg-[#1D5D9B] duration-1000">
-                <FaGithub size={24}/>
+              <a
+                href="https://github.com/codewithsaidul"
+                target="_blank"
+                className="text-[#1D5D9B] font-bold border border-[#1D5D9B] p-5 rounded-full hover:text-white hover:bg-[#1D5D9B] duration-1000"
+              >
+                <FaGithub size={24} />
               </a>
             </div>
           </div>
@@ -37,7 +55,7 @@ const Footer = () => {
               Company
             </h2>
 
-            <ul className="flex flex-col gap-2">
+            <ul className="flex flex-col gap-5">
               <li>
                 <a
                   href="#home"
@@ -96,6 +114,10 @@ const Footer = () => {
               </p>
             </div>
           </div>
+        </div>
+
+        <div>
+          <p className="text-base sm:text-lg md:text-xl lg:text-2xl text-gray-500 font-normal text-center mt-10 pt-10">&copy; 2023 Saku Estates by CodeWithSaidul</p>
         </div>
       </footer>
     </div>
