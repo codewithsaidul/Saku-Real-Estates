226024f9485c934f04f1a74b2972eb936fd98679
codewithsaidul <codewithsaidul@gmail.com>
2024-04-08 16:38:52 +0600

    About Component Added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import About from "../components/About";
 import Banner from "../components/Banner";
 // import BannerImg from '../assets/building.jpg'
 
@@ -6,6 +7,10 @@ const Home = () => {
     <div id="#home" className="w-full max-w-[1170px] mx-auto">
       <div className="mt-10">
         <Banner />
+      </div>
+
+      <div className="mt-20 mb-20">
+        <About/>
       </div>
     </div>
   );
