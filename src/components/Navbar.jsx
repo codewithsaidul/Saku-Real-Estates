1e6dd9baa0afee7675a9d6ac5915406588974bbc
codewithsaidul <codewithsaidul@gmail.com>
2024-04-09 15:58:11 +0600

    Navbar Updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -70,12 +70,48 @@ const Navbar = () => {
               {navLinks}
             </ul>
           </div>
-          <a className="text-base sm:text-2xl md:text-3xl font-bold text-[#1D5D9B]">SakuBliss Estates</a>
+          <a className="text-base sm:text-2xl md:text-3xl font-bold text-[#1D5D9B]">
+            SakuBliss Estates
+          </a>
         </div>
 
         <div className="navbar-end">
           <div className="hidden lg:flex">
             <ul className="menu menu-horizontal px-1">{navLinks}</ul>
+          </div>
+
+          <div>
+            <div className="dropdown dropdown-end">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost btn-circle avatar"
+              >
+                <div className="w-10 rounded-full">
+                  <img
+                    alt="Tailwind CSS Navbar component"
+                    src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                  />
+                </div>
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content mt-3 z-10 p-2 shadow bg-base-100 rounded-box w-52"
+              >
+                <li>
+                  <a className="justify-between">
+                    Profile
+                    <span className="badge">New</span>
+                  </a>
+                </li>
+                <li>
+                  <a>Settings</a>
+                </li>
+                <li>
+                  <a>Logout</a>
+                </li>
+              </ul>
+            </div>
           </div>
 
           <Link to="/login">
